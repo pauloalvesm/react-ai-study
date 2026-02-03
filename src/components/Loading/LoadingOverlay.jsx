866f9b3dbaf53cd0aee04f3b199a33a926443ea0
pauloalvesm <paulo.alves7351@gmail.com>
2026-02-03 16:07:35 -0300

    feat: create loading component

---
+++ src/components/Loading/LoadingOverlay.jsx
@@ -0,0 +1,9 @@
+import "./LoadingOverlay.css";
+
+export default function LoadingOverlay() {
+  return (
+    <div className="loading-overlay">
+      <div className="spinner"></div>
+    </div>
+  );
+}
